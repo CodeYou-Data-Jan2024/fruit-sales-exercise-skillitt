ecd1111fce32f467a50a6859ea3903facdcc79b6
skillitt <markdevore@outlook.com>
2024-04-27 14:12:04 -0400

    second edit

--- fruitsales.py
+++ fruitsales.py
@@ -7,9 +7,9 @@ data = {
 }
 index = ['2017 Sales', '2018 Sales']
 
-# Create a Pandas DataFrame
+#Pandas DataFrame
 df = pd.DataFrame(data, index=index)
 
-# Write the DataFrame to a CSV file
+# Write to a CSV file
 df.to_csv('sales_data.csv', index=True)
 
